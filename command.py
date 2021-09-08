8b18a339859575bedf5b5cc56aaf160c266db54d
CLAISSE Julien <julien.claise@loria.fr>
2021-09-08 19:38:46 +0200

    ADD Last command in game

--- command.py
+++ command.py
@@ -18,3 +18,31 @@ class Shot(AbstractCommand):
 class LoadShot(AbstractCommand):
     def run(self, x, y):
         self.move.hold(x,y)
+
+class HitBall(AbstractCommand):
+    def run(self):
+        self.move.line(0.5, 0.1, 0.5, 0.9)
+
+class Jump(AbstractCommand):
+    def run(self):
+        self.move.line(0.5, 0.9, 0.5, 0.1)
+
+class Counter(AbstractCommand):
+    def run(self, x, y):
+        self.move.hold(x,y)
+
+class RealeaseCounter(AbstractCommand):
+    def run(self, x, y):
+        self.move.realease()
+
+class Pass(AbstractCommand):
+    def run(self, dir_x, dir_y):
+        self.move.line(0.5 - dir_x,0.5 - dir_y, 0.5 + dir_x, 0.5 + dir_y)
+
+class UseItem(AbstractCommand):
+    def run(self, dir_x, dir_y):
+        self.move.line(0.5 - dir_x,0.5 - dir_y, 0.5 + dir_x, 0.5 + dir_y)
+
+class ShadowStep(AbstractCommand):
+    def run(self, dir_x):
+        self.move.line(-dir_x + 0.5,0.5, dir_x + 0.5, 0.5)
